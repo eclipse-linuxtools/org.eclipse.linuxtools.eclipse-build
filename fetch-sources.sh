66c63974f83ca4c1468d450ceb878aa45db4d61c
Andrew Overholt <overholt@redhat.com>
2008-11-19 16:02:00 +0000

    2008-11-19  Andrew Overholt  <overholt@redhat.com>
    	* generatebuild.xml: Fix path to include bootstrap/plugins.
    	* pdebuild.xml: Move things to SDK directory.
    	* fetch-sources.sh: Likewise.
    	* bootstrap: New directory.
    	* bootstrap/configuration/config.ini: Fix up osgi.bundles and set osgi.install.area.
    	* eclipse.ini: Remove for now (we don't need it).

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -2,9 +2,10 @@
 
 fileName=eclipse-sourceBuild-srcIncluded-3.5M3.zip
 
-if [ ! -f $fileName ] 
+if [ ! -f $fileName ]
 then
 	wget "http://download.eclipse.org/eclipse/downloads/drops/S-3.5M3-200810301917/$fileName"
 fi
 
-unzip -q $fileName
+mkdir -p SDK
+unzip -d SDK -q $fileName
