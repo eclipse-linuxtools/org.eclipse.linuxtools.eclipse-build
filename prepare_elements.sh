c6a0948bc584e7eb7df41be3a3fa3f6cd1b14f21
Alexander Kurtakov <akurtako@redhat.com>
2008-12-03 11:58:00 +0000

    * TODO: Add todo about equinox.http.jetty bundle name. * build.xml: Switch default task to one step further generateBuildScripts. * prepare_elements.sh: Skip equinox.http.jetty bundle. * test.properties: Add properties needed for compilation.

--- prepare_elements.sh
+++ prepare_elements.sh
@@ -7,6 +7,9 @@ for i in $(ls -d */); do
 	else
 		plugins=$plugins", "
 	fi
+	if [ "${i%%/}" = "org.eclipse.equinox.http.jetty_1.1.0" ]; then
+		continue
+	fi
 	plugins=$plugins"plugin@"${i%%/}; 
 done
 echo $plugins
