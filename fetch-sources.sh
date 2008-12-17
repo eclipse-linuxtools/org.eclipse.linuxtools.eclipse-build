a2fbd21d62a904ad3a37dbefa38092837305ab4c
Alexander Kurtakov <akurtako@redhat.com>
2008-12-17 12:09:14 +0000

    jump to 3.5M4

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-fileName=eclipse-sourceBuild-srcIncluded-3.5M3.zip
+fileName=eclipse-sourceBuild-srcIncluded-3.5M4.zip
 
 if [ ! -f $fileName ]
 then
-	wget "http://download.eclipse.org/eclipse/downloads/drops/S-3.5M3-200810301917/$fileName"
+	wget "http://download.eclipse.org/eclipse/downloads/drops/S-3.5M4-200812111908/$fileName"
 fi
 
 rm -fr SDK
