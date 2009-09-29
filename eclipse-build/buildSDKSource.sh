1f80f29899faccf5dc6b3ac9423227f1ee51d56d
Andrew Overholt <overholt@redhat.com>
2009-09-29 21:34:41 +0000

    2009-09-29  Andrew Overholt  <overholt@redhat.com>
    	* buildSDKSource.sh: Update for 3.5.1.  Use local Orbit and ECF dumps if
    	present.  Check out a version of eclipsebuilder (not HEAD).  Don't generate
    	bootstrap build.xml files as part of source fetching.  Fetch ECF sources.
    	* build.xml: Use testsBuildLabel and p2.director.version properties from
    	build.properties.
    	* build.properties: Move testsBuildLabel and p2.director.version here.
    	Update default buildId.
    	* patches/eclipse-useLocalECFBundles.patch: Initial check-in. Use local ECF
    	bundles instead of hitting eclipse.org where possible.
    	* patches/eclipse-dontusefullmoonformaster.patch: Initial check-in. Don't use
    	internal IBM mirror.
    
    2009-09-25  Andrew Overholt  <overholt@redhat.com>
    
    	* eclipse.desktop: Remove Encoding
    	(see http://standards.freedesktop.org/desktop-entry-spec/1.0/apc.html).

--- eclipse-build/buildSDKSource.sh
+++ eclipse-build/buildSDKSource.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # Possible optimizations:
 # - don't check out non-Linux fragments (will need to patch features for this)
 
 baseDir=$(pwd)
 workDirectory=
-buildID=I20090611-1540
+buildID=R3_5_1
 baseBuilder=
 eclipseBuilder=
 baseBuilderTag="R3_5"
+eclipseBuilderTag="R3_5_1"
 fetchTests="no"
-eclipse-buildTag="R0_0_1"
+orbitRepoZip=orbitRepo-R20090825191606.zip
+ecfBundlesZip=ecf-R3_5_1.zip
+ecfTag="v20090604-1131"
 
 usage="usage:  <build ID> [-workdir <working directory>] [-baseBuilder <path to org.eclipse.releng.basebuilder checkout>] [-eclipseBuilder <path to org.eclipse.releng.eclipsebuilder checkout>] [-baseBuilderTag <org.eclipse.releng.basebuilder tag to check out>] [-noTests]"
 
@@ -22,6 +25,7 @@ do
                 -baseBuilder) baseBuilder="$2"; shift;;
                 -baseBuilderTag) baseBuilderTag="$2"; shift;;
                 -eclipseBuilder) eclipseBuilder="$2"; shift;;
+                -eclipseBuilderTag) eclipseBuilderTag="$2"; shift;;
                 -noTests) fetchTests="no"; shift;;
                 -help) echo $usage; exit 0;;
                 --help) echo $usage; exit 0;;
@@ -33,7 +37,7 @@ done
 
 # Must specify a build ID
 if [ "x${buildID}x" = "xx" ]; then
-    echo >&2 "Must specify build ID.  Example:  I20090611-1540 ."
+    echo >&2 "Must specify build ID.  Example:  R3_5_1 ."
     echo >&2 "${usage}"
     exit 1
 else
@@ -77,9 +81,48 @@ fi
 if [ ! -e ${eclipseBuilder} ]; then
   mkdir -p "${eclipseBuilder}"
   cd "${eclipseBuilder}"/..
-  cvs -d${cvsRepo} co org.eclipse.releng.eclipsebuilder
-  cd org.eclipse.releng.eclipsebuilder
+  cvs -d${cvsRepo} co -r ${eclipseBuilderTag} org.eclipse.releng.eclipsebuilder
+  cd "${eclipseBuilder}"
   patch -p0 < "${baseDir}"/patches/eclipse-addFetchMasterAndTestsTargets.patch
+  cd "${baseDir}"
+fi
+
+if [ -e ${orbitRepoZip} ]; then
+  if [ ! -e ${fetchDirectory}/orbitRepo ]; then
+    # Unzip Orbit repo
+    # Note:  This is a zip of the repository available at:
+    #
+    #  http://download.eclipse.org/tools/orbit/downloads/drops/R20090825191606/updateSite
+    #
+    #  temporarily available at:
+    #
+    #  http://build.eclipse.org/eclipse/e4/build/e4/downloads/drops/4.0.0/targets/orbit/orbitRepo-R20090825191606.zip
+    #
+    # To avoid issues with mirrors that were out of sync with the repo, we
+    # use this zipped copy.  Thanks to Paul Webster for help generating
+    # it.  This is only done if we have a local copy of it.
+    mkdir -p ${fetchDirectory}/orbitRepo
+    unzip -q -d ${fetchDirectory}/orbitRepo ${orbitRepoZip}
+  fi
+fi
+
+# Due to timeouts with eclipse.org, we keep a zipped copy of the binary
+# ECF jars used y p2
+if [ -e ${ecfBundlesZip} ]; then
+  if [ ! -e ${fetchDirectory}/ecfBundles ]; then
+    unzip -q -d ${fetchDirectory} ${ecfBundlesZip}
+  fi
+fi
+
+if [ -e ${fetchDirectory}/orbitRepo ]; then
+  cd "${eclipseBuilder}"
+  patch -p0 < "${baseDir}"/patches/eclipse-dontusefullmoonformaster.patch
+  cd "${baseDir}"
+fi
+
+if [ -e ${fetchDirectory}/ecfBundles ]; then
+  cd "${eclipseBuilder}"
+  patch -p0 < "${baseDir}"/patches/eclipse-useLocalECFBundles.patch
   cd "${baseDir}"
 fi
 
@@ -104,19 +147,48 @@ fetchMasterFeature \
 -Duser.home="${homeDirectory}" \
 2>&1 | tee ${workDirectory}/sourcesFetch.log
 
-java -jar \
-"${baseBuilder}"/plugins/org.eclipse.equinox.launcher_*.jar \
--consolelog \
--data "${workspace}" \
--application org.eclipse.ant.core.antRunner \
--f ../pdebuild.xml generateScripts \
--DbuildDirectory="${fetchDirectory}" \
--DskipBase=true \
--Duser.home="${homeDirectory}" \
--DsdkSource="${fetchDirectory}" \
-2>&1 | tee "${workDirectory}"/generatePdeBuildScripts.log
+cd "${fetchDirectory}"
+
+mkdir ecf-src
+
+# Source for ECF bits that aren't part of SDK map files
+for f in \
+    org.eclipse.ecf \
+    org.eclipse.ecf.filetransfer \
+    org.eclipse.ecf.identity \
+    org.eclipse.ecf.ssl \
+; do
+cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/rt \
+export -r ${ecfTag} org.eclipse.ecf/framework/bundles/$f;
+done
+
+mv org.eclipse.ecf/framework/bundles/* ecf-src
+rm -fr org.eclipse.ecf/framework
+
+for f in \
+    org.eclipse.ecf.provider.filetransfer \
+    org.eclipse.ecf.provider.filetransfer.httpclient \
+    org.eclipse.ecf.provider.filetransfer.httpclient.ssl \
+    org.eclipse.ecf.provider.filetransfer.ssl \
+; do
+cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/rt \
+export -r ${ecfTag} org.eclipse.ecf/providers/bundles/$f;
+done
+
+mv org.eclipse.ecf/providers/bundles/* ecf-src
+rm -fr org.eclipse.ecf
+
+cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/rt \
+export -r ${buildID} org.eclipse.equinox/components/bundles/org.eclipse.equinox.concurrent;
+
+mv org.eclipse.equinox/components/bundles/* ecf-src
+rm -rf org.eclipse.equinox
 
 cd "${fetchDirectory}"
+# We don't want to re-ship these as those bundles inside will already be
+# copied into the right places for the build
+rm -rf ecfBundles orbitRepo
+
 mkdir eclipse-${buildID}-fetched-src
 mv * eclipse-${buildID}-fetched-src
 tar cjf "${workDirectory}"/eclipse-${buildID}-fetched-src.tar.bz2 \
@@ -151,8 +223,6 @@ mv * eclipse-sdktests-${buildID}-fetched-src
 tar cjf "${workDirectory}"/eclipse-sdktests-${buildID}-fetched-src.tar.bz2 \
   eclipse-sdktests-${buildID}-fetched-src
 
-fi
-
 # Testing runtests and test.xml scripts which are not in org.eclipse.test
 cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse co \
   -r v${buildID} \
@@ -166,5 +236,7 @@ rm -rf org.eclipse.releng.eclipsebuilder
 tar cjf \
   "${workDirectory}"/eclipse-sdktests-${buildID}-fetched-scripts.tar.bz2 \
   ${testScripts}
+
+fi
 
 cd "${baseDir}"
