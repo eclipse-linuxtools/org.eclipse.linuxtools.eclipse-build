c2c7a70bed6f747e785e0d494e59172c9fd2414d
Alexander Kurtakov <akurtako@redhat.com>
2009-10-19 10:44:20 +0000

    2009-10-19  Alexander Kurtakov  <akurtako@redhat.com>
    	* eclipse-build-additionalArchs.tar.bz2: New file.
    	* additionalArchs/rename.sh: New file.
    	* patches/eclipse-add-ppc64-filesystem.patch: New file.
    	* patches/eclipse-add-ppc64-swt.patch: New file.
    	* build.xml: Add ppc64 support.

---
+++ eclipse-build/additionalArchs/rename.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+newArch=$1
+
+function moves(){
+for f in $(find -type d); do
+      tofile=$(echo $f | sed "s/ppc/$newArch/")
+      if [ $tofile != $f ]; then
+        cp -pfr $f $tofile
+      fi
+    done
+for f in $(find -type f); do
+      tofile=$(echo $f | sed "s/ppc/$newArch/")
+      if [ $tofile != $f ]; then
+        cp -pfr $f $tofile
+        rm -fr $f
+      fi
+    done
+}
+
+cd org.eclipse.equinox.launcher.gtk.linux.$newArch
+    moves
+cd ..
+cd org.eclipse.core.filesystem.linux.$newArch
+    moves
+cd ..
+cd org.eclipse.swt.gtk.linux.$newArch
+    moves
+cd ..
