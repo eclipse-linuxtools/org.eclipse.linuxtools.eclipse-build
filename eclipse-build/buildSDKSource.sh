c967cdc42465a4061559efc8faf525ecb6caeed2
Andrew Overholt <overholt@redhat.com>
2010-03-08 19:48:34 +0000

    2010-03-08  Andrew Overholt  <overholt@redhat.com>
    	* buildSDKSource.sh: Revert to fetching from CVS ourselves to get
    	featureVersions.properties and pluginVersions.properties.

--- eclipse-build/buildSDKSource.sh
+++ eclipse-build/buildSDKSource.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 set -e
 
-# We use the upstream srcIncluded drop as input.  It can be obtained
-# from:   http://download.eclipse.org/eclipse/downloads/drops/
-#
-# In our case, we're using:  R-3.5.2-201002111343/eclipse-sourceBuild-srcIncluded-3.5.2.zip
-
-equinoxTag="R35x_v20100209"
-ecfTag="v20090831-1453"
-label="3.5.2"
-buildID="R3_5_2"
-upstreamSrcDrop="/tmp/eclipse-sourceBuild-srcIncluded-${label}.zip"
-
-eclipsebuildURL=svn://dev.eclipse.org/svnroot/technology/org.eclipse.linuxtools/eclipse-build/branches/symlinkDependencies
-
-baseBuilderTag="R3_5"
-
 baseDir=$(pwd)
 workDirectory=
 baseBuilder=
 eclipseBuilder=
-fetchTests="no"
-buildTests="yes"
 
-usage="usage:  <build ID> [-workdir <working directory>] [-upstreamSrcDrop <path to upstream srcIncluded zip>] [-baseBuilder <path to org.eclipse.releng.basebuilder checkout>] [-baseBuilderTag <org.eclipse.releng.basebuilder tag to check out>] [-noTests]"
+buildID="R3_5_2"
+baseBuilderTag="R3_5"
+eclipseBuilderTag="R3_5_2"
+label="3.5.2"
+fetchTests="yes"
+equinoxTag="R35x_v20100209"
+ecfTag="v20090831-1453"
+
+usage="usage:  <build ID> [-workdir <working directory>] [-baseBuilder <path to org.eclipse.releng.basebuilder checkout>] [-eclipseBuilder <path to org.eclipse.releng.eclipsebuilder checkout>] [-baseBuilderTag <org.eclipse.releng.basebuilder tag to check out>] [-noTests]"
 
 while [ $# -gt 0 ]
 do
@@ -32,40 +23,107 @@ do
                 -workDir) workDirectory="$2"; shift;;
                 -baseBuilder) baseBuilder="$2"; shift;;
                 -baseBuilderTag) baseBuilderTag="$2"; shift;;
-                -upstreamSrcDrop) upstreamSrcDrop="$2"; shift;;
+                -eclipseBuilder) eclipseBuilder="$2"; shift;;
+                -eclipseBuilderTag) eclipseBuilderTag="$2"; shift;;
                 -noTests) fetchTests="no"; shift;;
                 -help) echo $usage; exit 0;;
                 --help) echo $usage; exit 0;;
                 -h) echo $usage; exit 0;;
+                *) buildID="$1";
         esac
         shift
 done
+
+# Must specify a build ID
+if [ "x${buildID}x" = "xx" ]; then
+    echo >&2 "Must specify build ID.  Example:  R3_5_1 ."
+    echo >&2 "${usage}"
+    exit 1
+else
+  echo "Going to create source tarballs for ${buildID}."
+fi
 
 if [ "x${workDirectory}x" = "xx" ]; then
   workDirectory="${baseDir}"
   echo "Working directory not set; using this directory (${baseDir})."
 fi
 
-mkdir -p ${workDirectory}
-cd ${workDirectory}
+if [ "x${baseBuilder}x" = "xx" ]; then
+  baseBuilder="${workDirectory}"/org.eclipse.releng.basebuilder
+  echo "Basebuilder checkout not specified; will check out ${baseBuilderTag} into ${baseBuilder}."
+fi
+if [ "x${eclipseBuilder}x" = "xx" ]; then
+  eclipseBuilder="${workDirectory}"/org.eclipse.releng.eclipsebuilder
+  echo "Eclipsebuilder checkout not specified; will check out into ${eclipseBuilder}."
+fi
 
-# Cleanup
-rm -rf eclipse-${label}-src org.eclipse.releng.eclipsebuilder
+fetchDirectory="${workDirectory}"/fetch
+mkdir -p "${fetchDirectory}"
+homeDirectory="${workDirectory}"/userhome
+rm -rf "${homeDirectory}"
+mkdir -p "${homeDirectory}"
+workspace="${workDirectory}"/workspace
+rm -rf "${workspace}"
+mkdir -p "${workspace}"
+cvsRepo=":pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse"
+mapsRoot="org.eclipse.releng/maps"
 
-mkdir -p eclipse-${label}-src
-cd eclipse-${label}-src
+# Fetch basebuilder
+if [ ! -e "${baseBuilder}" ]; then
+  mkdir -p "${baseBuilder}"
+  cd "${baseBuilder}"/..
+  cvs -d${cvsRepo} co -r ${baseBuilderTag} org.eclipse.releng.basebuilder
+  cd "${baseDir}"
+fi
 
-unzip -d upstream -q ${upstreamSrcDrop}
-mv upstream/org.eclipse.releng.eclipsebuilder ..
-eclipseBuilder=$(pwd)/../org.eclipse.releng.eclipsebuilder
-mv upstream/src .
-rm -rf upstream
+# Fetch eclipsebuilder
+if [ ! -e ${eclipseBuilder} ]; then
+  mkdir -p "${eclipseBuilder}"
+  cd "${eclipseBuilder}"/..
+  cvs -d${cvsRepo} co -r ${eclipseBuilderTag} org.eclipse.releng.eclipsebuilder
+  cd "${eclipseBuilder}"
+  patch -p0 < "${baseDir}"/patches/eclipse-addFetchMasterAndTestsTargets.patch
+  cd "${baseDir}"
+fi
 
-cd src
+if [ -e ${fetchDirectory}/orbitRepo ]; then
+  cd "${eclipseBuilder}"
+  patch -p0 < "${baseDir}"/patches/eclipse-dontusefullmoonformaster.patch
+  cd "${baseDir}"
+fi
 
-# Source for ECF bits that aren't part of SDK source drop
+if [ -e ${fetchDirectory}/ecfBundles ]; then
+  cd "${eclipseBuilder}"
+  patch -p0 < "${baseDir}"/patches/eclipse-useLocalECFBundles.patch
+  cd "${baseDir}"
+fi
+
+# Build must be run from within o.e.r.eclipsebuilder checkout
+cd "${eclipseBuilder}"
+
+java -jar \
+"${baseBuilder}"/plugins/org.eclipse.equinox.launcher_*.jar \
+-consolelog \
+-data "${workspace}" \
+-application org.eclipse.ant.core.antRunner \
+-f buildAll.xml \
+fetchMasterFeature \
+-DbuildDirectory="${fetchDirectory}" \
+-DskipBase=true \
+-DmapsRepo=${cvsRepo} \
+-DmapCvsRoot=${cvsRepo} \
+-DmapsCvsRoot=${cvsRepo} \
+-DmapsRoot=${mapsRoot} \
+-DmapsCheckoutTag=${buildID} \
+-DmapVersionTag=${buildID} \
+-Duser.home="${homeDirectory}" \
+2>&1 | tee ${workDirectory}/sourcesFetch.log
+
+cd "${fetchDirectory}"
+
 mkdir ecf-src
 
+# Source for ECF bits that aren't part of SDK map files
 for f in \
     org.eclipse.ecf \
     org.eclipse.ecf.filetransfer \
@@ -93,77 +151,51 @@ mv org.eclipse.ecf/providers/bundles/* ecf-src
 rm -fr org.eclipse.ecf
 
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/rt \
-export -r ${equinoxTag} org.eclipse.equinox/components/bundles/org.eclipse.equinox.concurrent;
+export -r ${buildID} org.eclipse.equinox/components/bundles/org.eclipse.equinox.concurrent;
 
 mv org.eclipse.equinox/components/bundles/* ecf-src
 rm -rf org.eclipse.equinox
 
-# Get rid of src subdirectory
-cd ..
-mv src/* .
+cd "${fetchDirectory}"
+# We don't want to re-ship these as those bundles inside will already be
+# copied into the right places for the build
+rm -rf ecfBundles orbitRepo
 
 # Remove files from the version control system
 find -depth -name CVS -exec rm -rf {} \;
 
 # Remove prebuilt binaries
 find \( -name '*.exe' -o -name '*.dll' \) -delete
-find \( -name '*.jnilib' -o -name '*.sl' \) -delete
-find \( -name '*.a' -o -name '*.so' -o -name '*.so.2' \) -delete
+find \( -name '*.so' -o -name '*.so.2' -o -name '*.a' \) -delete
+find \( -name '*.sl' -o -name '*.jnilib' \) -delete
 find features/org.eclipse.equinox.executable -name eclipse -delete
 find \( -name '*.cvsignore' \) -delete
+
+# Remove unnecessary repo
+rm -rf tempSite
 
 # Remove binary JARs
 find -name '*.jar' -delete
 
-# Remove pre-created .source bundles/features
-rm -rf plugins/*\.source_*
-rm -rf features/*\.source
+# Remove fetch logs
+rm fetch_*
+
+# Remove unnecessary feature and plugins
+rm -rf features/org.eclipse.sdk.examples
+rm -rf plugins/*.examples*
 
 # Remove empty directories
-find -depth -type d -empty -delete
+find -type d -empty -delete
 
 cd ..
-tar cjf eclipse-${label}-src.tar.bz2 eclipse-${label}-src
+mv fetch eclipse-${label}-src
+tar cjf "${workDirectory}"/eclipse-${label}-src.tar.bz2 \
+  eclipse-${label}-src
+cd "${eclipseBuilder}"
 
-#-------------- / Tests / --------------------#
 if [ "${fetchTests}" = "yes" ]; then
 
-homeDirectory="${workDirectory}"/userhome
-rm -rf "${homeDirectory}"
-mkdir -p "${homeDirectory}"
-workspace="${workDirectory}"/workspace
-rm -rf "${workspace}"
-mkdir -p "${workspace}"
-cvsRepo=":pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse"
-mapsRoot="org.eclipse.releng/maps"
-
-pushd ${eclipseBuilder}
-patch -p0 < "${baseDir}"/patches/eclipse-addFetchMasterAndTestsTargets.patch
-popd
-
-# Fetch basebuilder
-if [ "x${baseBuilder}x" = "xx" ]; then
-  baseBuilder="${workDirectory}"/org.eclipse.releng.basebuilder
-  echo "Basebuilder checkout not specified; will check out ${baseBuilderTag} into ${baseBuilder}."
-fi
-
-if [ ! -e "${baseBuilder}" ]; then
-  mkdir -p "${baseBuilder}"
-  cd "${baseBuilder}"/..
-  cvs -d${cvsRepo} co -r ${baseBuilderTag} org.eclipse.releng.basebuilder
-  cd "${baseDir}"
-fi
-
-# Build must be run from within o.e.r.eclipsebuilder checkout
-cd ${eclipseBuilder}
-
-fetchDirectory="${workDirectory}"/fetch
-mkdir -p "${fetchDirectory}"
 rm -rf "${fetchDirectory}"/*
-
-fi
-
-if [ "${buildTests}" = "yes" ]; then
 
 java -jar \
 "${baseBuilder}"/plugins/org.eclipse.equinox.launcher_*.jar \
@@ -203,7 +235,5 @@ rm -rf org.eclipse.releng.eclipsebuilder
 tar cjf ${workDirectory}/eclipse-sdktests-${label}-scripts.tar.bz2 ${testScripts}
 
 fi
-#-------------- / tests / --------------------#
-rm -rf ${eclipseBuilder}
 
 cd "${baseDir}"
