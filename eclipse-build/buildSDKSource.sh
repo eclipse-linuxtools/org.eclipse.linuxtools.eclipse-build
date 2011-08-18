e6053a4a617f83a00b5112b514721dd66e780d3c
Sami Wagiaalla <swagiaal@redhat.com>
2011-08-18 13:32:12 -0400

    add tag I20110719-0800 to tarball labels

--- eclipse-build/buildSDKSource.sh
+++ eclipse-build/buildSDKSource.sh
@@ -6,10 +6,10 @@ workDirectory=
 baseBuilder=
 eclipseBuilder=
 
-buildID="R3_7"
+buildID="I20110719-0800"
 baseBuilderTag="v20110711"
 eclipseBuilderTag=""
-label="3.8.0"
+label="3.8.0-I20110719-0800"
 fetchTests="yes"
 
 usage="usage:  <build ID> [-workdir <working directory>] [-baseBuilder <path to org.eclipse.releng.basebuilder checkout>] [-eclipseBuilder <path to org.eclipse.releng.eclipsebuilder checkout>] [-baseBuilderTag <org.eclipse.releng.basebuilder tag to check out>] [-noTests]"
