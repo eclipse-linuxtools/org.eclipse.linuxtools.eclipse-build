73976e4ead133a7ee37348793c333aef316f12e4
Krzysztof Daniel <kdaniel@redhat.com>
2012-03-07 14:51:10 +0100

    Inline secondary fragments generation into build process, so it is no longer necessary to regenerate them.

--- eclipse-build/additionalArchs/rename.sh
+++ eclipse-build/additionalArchs/rename.sh
@@ -4,13 +4,13 @@ newArch=$1
 
 moves(){
 for f in $(find -type d); do
-      tofile=$(echo $f | sed "s/ppc/$newArch/")
+      tofile=$(echo $f | sed "s/x86_64/$newArch/")
       if [ $tofile != $f ]; then
         cp -pfr $f $tofile
       fi
     done
 for f in $(find -type f); do
-      tofile=$(echo $f | sed "s/ppc/$newArch/")
+      tofile=$(echo $f | sed "s/x86_64/$newArch/")
       if [ $tofile != $f ]; then
         cp -pfr $f $tofile
         rm -fr $f
