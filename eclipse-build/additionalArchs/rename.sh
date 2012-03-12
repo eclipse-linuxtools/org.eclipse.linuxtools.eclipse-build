e5989b0ffb8f39e1ab938f96f98a28fc11143d50
Krzysztof Daniel <kdaniel@redhat.com>
2012-03-12 17:56:00 +0100

    Change the rename.sh to use proper base architecture.
    Follow up of commit http://git.eclipse.org/c/linuxtools/org.eclipse.linuxtools.eclipse-build.git/commit/?h=cdaniel/inlineSecondaryArchs&id=3572425bd01a4837797c7757b653c14056d8d6f4

--- eclipse-build/additionalArchs/rename.sh
+++ eclipse-build/additionalArchs/rename.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-newArch=$1
+baseArch=$1
+newArch=$2
 
 moves(){
 for f in $(find -type d); do
-      tofile=$(echo $f | sed "s/x86_64/$newArch/")
+      tofile=$(echo $f | sed "s/$baseArch/$newArch/")
       if [ $tofile != $f ]; then
         cp -pfr $f $tofile
       fi
     done
 for f in $(find -type f); do
-      tofile=$(echo $f | sed "s/x86_64/$newArch/")
+      tofile=$(echo $f | sed "s/$baseArch/$newArch/")
       if [ $tofile != $f ]; then
         cp -pfr $f $tofile
         rm -fr $f
