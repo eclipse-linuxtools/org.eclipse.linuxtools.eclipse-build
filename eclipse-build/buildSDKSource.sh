6632d3377ad71126004f82fc3aea289923dc4008
Krzysztof Daniel <kdaniel@redhat.com>
2012-03-23 09:24:11 +0100

    Another attempt to fix patches.

--- eclipse-build/buildSDKSource.sh
+++ eclipse-build/buildSDKSource.sh
@@ -85,7 +85,7 @@ if [ ! -e ${eclipseBuilder} ]; then
   cd "${eclipseBuilder}"
   patch -p0 < "${baseDir}"/patches/eclipse-addFetchMasterAndTestsTargets.patch
   patch -p0 < "${baseDir}"/patches/e4-removeSkipMapsCheck.patch
-  patch -p1 < "${baseDir}"/patches/e4-doNotMirrorEMF.patch
+  patch -p0 < "${baseDir}"/patches/e4-doNotMirrorEMF.patch
   cd "${baseDir}"
 fi
 
