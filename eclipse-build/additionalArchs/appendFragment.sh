701400b0ca475ea73bd828c66b00fb63c5ec2c8c
Krzysztof Daniel <kdaniel@redhat.com>
2012-08-23 09:32:46 +0200

    Get only what necessary from the Eclipse CBI build.
    Eclipse Platform CBI build requires only a few things from eclipse-build, mostly
    some linux specific scripts (which needs to be installed).
    
    Additionally, some poms were added to build initializer.

--- eclipse-build/additionalArchs/appendFragment.sh
+++
@@ -1,2 +0,0 @@
-cat pluginVersions.properties | grep $1.$2, | sed "s/$2,/$3,/"
-cat pluginVersions.properties | grep $1.$2, | sed "s/$2,/$3,/" >> pluginVersions.properties
--- eclipse-build/buildSource.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-./buildSDKSource.sh
-./buildEclipseBuildSource.sh
